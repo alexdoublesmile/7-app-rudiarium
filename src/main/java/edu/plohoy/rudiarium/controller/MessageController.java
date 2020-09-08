94af1d2a7b39677216084e85d994c9fd37899b2a
Alex Plohoy <alexplohoy@mail.ru>
2020-09-08 19:28:21 +0300

    add data to controler

--- src/main/java/edu/plohoy/rudiarium/controller/MessageController.java
+++ src/main/java/edu/plohoy/rudiarium/controller/MessageController.java
@@ -1,5 +1,26 @@
 package edu.plohoy.rudiarium.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/messages")
 public class MessageController {
+
+    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
+       add(new HashMap<String, String>() {{put("id", "1"); put("text", "First Message");}});
+       add(new HashMap<String, String>() {{put("id", "2"); put("text", "Second Message");}});
+       add(new HashMap<String, String>() {{put("id", "3"); put("text", "Third Message");}});
+    }};
+    
+    @GetMapping
+    public List<Map<String, String>> list() {
+        return messages;
+    }
 }
