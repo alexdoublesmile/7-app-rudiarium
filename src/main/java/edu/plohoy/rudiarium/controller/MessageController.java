2fe6236f90d6bceb983804140861384f6e2488e7
Alex Plohoy <alexplohoy@mail.ru>
2020-09-08 19:39:29 +0300

    refactor MessageController: getmessage() -> getOne()

--- src/main/java/edu/plohoy/rudiarium/controller/MessageController.java
+++ src/main/java/edu/plohoy/rudiarium/controller/MessageController.java
@@ -27,7 +27,7 @@ public class MessageController {
     }
 
     @GetMapping("/{id}")
-    public Map<String, String> getMessage(@PathVariable String id) {
+    public Map<String, String> getOne(@PathVariable String id) {
         return messages.stream()
                 .filter(message -> message.get("id").equals(id))
                 .findFirst()
